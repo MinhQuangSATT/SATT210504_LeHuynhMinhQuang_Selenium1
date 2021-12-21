0c6fb08fc5536ea171486523087ed33b69c5b3ce
MinhQuangSATT <SATT21.05.04@logigear.com>
2021-12-21 12:12:05 +0700

    Added Page Objects , Constant folder

---
+++ src/main/java/Constant/Constant.java
@@ -0,0 +1,9 @@
+package Constant;
+import org.openqa.selenium.WebDriver;
+
+public class Constant {
+    public static WebDriver WEBDRIVER;
+    public static final String RAILWAY_URL = "http://www.railway.somee.com/Account/Login.cshtml";
+    public static final String USERNAME = "hehehe@mailinator.com";
+    public static final String PASSWORD = "123456789";
+}
